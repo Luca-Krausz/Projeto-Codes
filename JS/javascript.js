bf9a19e1c61d10aa23cfe66f7746a87ea61eca42
Luca-Krausz <Krausk225@gmail.com>
2023-12-01 17:08:11 -0300

    Bug Fixes #1 + Vender 1.0

--- JS/javascript.js
+++ JS/javascript.js
@@ -5,6 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const sendButton = document.getElementById('sendButton');
     const messageArea = document.getElementById('messageArea');
 
+    input.addEventListener('keypress', function(event) {
+        if (event.key === "Enter") {
+            const messageText = input.value.trim();
+            if (messageText) {
+                createMessageBubble(messageText);
+                input.value = '';
+                event.preventDefault(); 
+            }
+        }
+    });
+    
     sendButton.addEventListener('click', function() {
         const messageText = input.value.trim();
         if (messageText) {
@@ -17,6 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
         const messageBubble = document.createElement('li');
         messageBubble.textContent = text;
         messageBubble.classList.add('message-bubble');
-        messageArea.appendChild(messageBubble);
+        messageArea.prepend(messageBubble);
+        messageArea.scrollTop = 0;
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var postar = document.querySelector('.postar');
+
+    posta.addEventListener('click', function() {
+
+        var mais = document.getElementById('botao-mais');
+    
+
+
+    });
+
 });
